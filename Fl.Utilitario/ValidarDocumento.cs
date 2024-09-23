a6b4742d05a8a325b94979f1b699c916a915142c
Cleisson1992 <cleissonvasconcelos1992@hotmail.com>
2024-09-23 20:30:10 -0300

    Adicionar arquivos de projeto.

---
+++ Fl.Utilitario/ValidarDocumento.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Fl.Utilitario
+{
+    public static class ValidarDocumento
+    {
+        /// <summary>
+        /// Valida o CPF informado.
+        /// </summary>
+        /// <param name="cpf">CPF em formato de string (somente números).</param>
+        /// <returns>Retorna true se o CPF for válido, caso contrário, false.</returns>
+        public static bool ValidarCPF(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf)) return false;
+
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11 || cpf.All(c => c == cpf[0]))
+                return false;
+
+            var multiplicadores1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicadores2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            var tempCpf = cpf.Substring(0, 9);
+            var soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicadores1[i];
+
+            var resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            var digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicadores2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+    }
+}
